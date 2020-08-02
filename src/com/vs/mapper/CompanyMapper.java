056e92d38924fa5feb0fdc81d81c24c93ee04695
hanyena <gksdpsk2021@gmail.com>
2020-08-02 23:09:37 +0900

    [add] DB select Mapper 추가
    - COMPANY, INDUSTRY, STOCK_RECORDS 테이블에 대한 select mapper 추가

--- src/com/vs/mapper/CompanyMapper.java
+++ src/com/vs/mapper/CompanyMapper.java
@@ -1,5 +1,14 @@
 package com.vs.mapper;
 
-public interface CompanyMapper {
+import java.util.List;
+import org.apache.ibatis.annotations.Select;
+import com.vs.vo.CompanyVO;
 
+public interface CompanyMapper {
+	@Select("select * from COMPANY")
+	public List<CompanyVO> selectCompany(); 
+	
+	@Select("select * from COMPANY where stock_code=#{stock_code}")
+	public CompanyVO searchCompany(String stock_code);
+	
 }
