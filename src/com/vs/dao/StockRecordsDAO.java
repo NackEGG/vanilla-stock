05deacb38853268c58e496a3233786d377044c5e
unknown <tjrco56@naver.com>
2020-08-03 09:21:41 +0900

    프로시져 수정 및 DAO수정

--- src/com/vs/dao/StockRecordsDAO.java
+++ src/com/vs/dao/StockRecordsDAO.java
@@ -1,10 +1,11 @@
 package com.vs.dao;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.vs.vo.StockRecordsVO;
 
 public interface StockRecordsDAO {
-	public List<StockRecordsVO> selectList();
-	public StockRecordsVO select();
+	public List<StockRecordsVO> selectList(String StockCode, Date tDate);
+	public StockRecordsVO select(String StockCode, Date tDate);
 }
