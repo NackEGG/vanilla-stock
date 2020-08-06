58fd54b325cde22d3155d38f454746cca6dcf00b
kim-dabin <pippisgoing@gmail.com>
2020-08-06 15:51:44 +0900

    add util/PaginateUtil.java

--- src/com/vs/dao/StockRecordsDAO.java
+++ src/com/vs/dao/StockRecordsDAO.java
@@ -11,8 +11,6 @@ public interface StockRecordsDAO {
 
 	public List<StockRecordsVO> selectList(StockRecordsVO stockRecordsVO);
 	public StockRecordsVO select(StockRecordsVO stockRecordsVO);
-	public List<StockRecordsVO> selectList(String StockCode, Date tDate);
-	public StockRecordsVO select(String StockCode, Date tDate);
 	public List<Pair<StockRecordsVO, String>> selectStockWithCompany(String startDate, String endDate);
 
 }
